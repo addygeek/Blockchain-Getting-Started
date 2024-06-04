c38c309cb23da2612f5a1d2c8953b6819ce04bca
addygeek <ui22cs03@iiitsurat.ac.in>
2024-06-04 23:43:08 +0530

    added the wallet feature and updated the valididty of transcation, now can create the coin and do trasactions

--- Blockchain.js
+++ Blockchain.js
@@ -3,6 +3,14 @@ const crypto = require('crypto');
 //Creating a function to calculate the SHA256 hash of a message
 SHA256 = message => crypto.createHash('sha256').update(message).digest('hex');
 
+//importing Elliptic  for wallet creating 
+const EC = require('elliptic').ec, ec = new EC('secp256k1');
+//Lets configure the WALLET
+const MINT_WALLET = ec.genKeyPair();
+
+const MINT_PUBLIC_ADDRESS = MINT_WALLET.getPublic('hex');
+const MINT_PRIVATE_ADDRESS = MINT_WALLET.getPrivate('hex');
+
 class Block {
     constructor(data= [])
     {
@@ -24,7 +32,16 @@ class Block {
             this.hash = this.getHash();
         }
     }
+    //it checks the recent transcation performed was valid or not
+    hasValidTransactions (chain)
+    {
+        return this.data.every(transaction => transaction.isValid(transaction,chain));
+    }
+
+
 }
+
+
 // Now lets test it
 const block1 = new Block(['Transaction 1']);
 console.log(block1);
@@ -38,6 +55,8 @@ class Blockchain {
         this.chain= [new Block(['Genesis Block'])];
         this.difficulty= 2;
         this.blockTime =5000;
+        this.Transaction = [];
+        this.reward = 10;
     }
     //retrive the last added block where next block will get linked as previous block
     getLastBlock()
@@ -61,15 +80,56 @@ class Blockchain {
         {
             const currentblock = this.chain[i];
             const prevBlock = this.chain[i-1];
-            if(currentblock.hash !== currentblock.getHash() || currentblock.prevHash !== prevBlock.hash)
+            if(currentblock.hash !== currentblock.getHash() || 
+            currentblock.prevHash !== prevBlock.hash ||
+            !currentblock.hasValidTransactions(this)){
+                return false;
+            }
+        
             {
                 return false;
             }
         }
         return true;
     }
+    //transcationg adding in blockchain
+    addTransaction(transaction)
+    {
+        if(transaction.isValid(transaction,this))
+            {
+                this.Transaction.push(transaction);
+            }
+        
+    }
+    //minting is like printing new coins as reward to miners
+    mineTransaction(rewardAddress)
+    {   const rewardTransaction = new Transaction(MINT_PUBLIC_ADDRESS,rewardAddress,this.reward);
+        this.addBlock(new Block([rewardTransaction,...this.Transaction]));
+        this.transaction=[];
 
-}
+    }
+
+    //Adding the feature of see the balance of particular user & block
+    getBalance(address)
+    {
+     let balance= 0;
+     
+     this.chain.forEach(block => {
+            block.data.forEach(transaction => {
+                if(transaction.from === address)
+                {
+                    balance -= transaction.amount;
+                }
+                if(transaction.to === address)
+                {
+                    balance += transaction.amount;
+                }
+            });
+     })
+     return balance;
+
+    }
+
 //Lets now make a Blockchain on my name and then add the transcation mannually
 const ADDYCOIN = new Blockchain();
 ADDYCOIN.addBlock(new Block(['Transaction 1']));
@@ -117,3 +177,86 @@ console.log("Is ADDY Coin valid?", ADDYCOIN.isValid());
 ADDYCOIN.chain[1].data= "This is hacked"
 console.log(ADDYCOIN.chain);
 console.log(ADDYCOIN.isValid());
+
+
+class Transaction{
+    constructor(from, to, amount)
+    {
+        this.from= from;
+        this.to= to;
+        this.amount= amount;
+    }
+    sign(keyPair)
+    {
+        //Check if the keyPair is the sender of the transaction
+        if(keyPair.getPublic('hex') === this.from)
+        {
+            //it approve the transaction is sign by original iniciator
+            this.signature = keyPair.sign(SHA256(this.from + this.to + this.amount)).toDER('hex');
+        }
+    }
+    isValid(tx,chain)
+    {
+        return (
+            tx.from &&
+            tx.to &&
+            tx.amount &&
+            chain.getBalance(tx.from)>= tx.amount &&
+            ec.keyFromPublic(tx.from,'hex').verify(SHA256(tx.from + tx.top+  tx.amount), tx.signature)
+        )
+    }
+}
+
+//check the Wallet
+console.log("Public: ",MINT_PUBLIC_ADDRESS);
+console.log("Private: ",MINT_PRIVATE_ADDRESS);
+//OUTPUT:- 
+//Public:  04e43a39f6efd7e4791abb347da764cc36f0a926a10cb011d5e9e4d0666459cd1255e9c19bae9dbdadd18baaba68bf2ed7898d36710118b8fb4201c40bd244fb9f
+//Private:  841decaf4649cb047d7d20f991d034908f92e4ea073cf71597cf051c1fa65261
+// NOTE:- In real lie on Block-chain never reveal your private key to anyone else you lose your crypto
+
+
+//Lets now Create Wallet to send and recieve the amount
+
+const ADITYA_WALLET = ec.genKeyPair();
+const RECRUTER_WALLET = ec.genKeyPair();
+const MINER_WALLET = ec.genKeyPair();
+//Now created the blockchain of coin name below
+const SathoshiCoin = new Blockchain();
+
+//Lets do the transaction on blockchain
+const transaction = new Transaction(ADITYA_WALLET.getPublic('hex'), RECRUTER_WALLET.getPublic('hex'), 100);
+transaction.sign(ADITYA_WALLET);
+SathoshiCoin.addTransaction(transaction);
+SathoshiCoin.mineTransaction(MINER_WALLET.getPublic('hex'));
+
+//lts check the SathoshiCoin
+console.log(SathoshiCoin.chain);
+//Output:- 
+//     Block {
+//         timestamp: 1717523270316,
+//         data: [ 'Genesis Block' ],
+//         hash: '8d17cbd3497d110ed84820db9c923bebc4b4ede3235e9731b95c665bb25e77a8',
+//         prevHash: '',
+//         nonce: 0
+//       },
+//       Block {
+//         timestamp: 1717523270371,
+//         data: [ [Transaction], [Transaction] ],
+//         hash: '0096ac8e84ed471ecbdb7ba9dd3cecbb53792bb6481f5c48f668987cb91dd0d9',
+//         prevHash: '8d17cbd3497d110ed84820db9c923bebc4b4ede3235e9731b95c665bb25e77a8',
+//         nonce: 663
+//       }
+//     ]
+//let study what those transactions in data are ;
+console.log(SathoshiCoin.chain[1].data[1]);
+// Below here the from is address of ADITYA_WALLET and then to is the RECUTOR WALLET and amount of coin trasfer
+//console.log(ADITYA_WALLET.getPublic('hex'));
+// from: '04387c1dcb5ba3fda8bc5bf750c04d0c7d82c2ff74d653fe7f1fd7e312a2df30926b5c3b3fd3f4bc5046622873eaa26eae8f53882d850d0473f1017ac266ef57a7',
+//   to: '041fa35db437c848318a66de6fb81bce260c0672a498dbe32bf9667331adeeaee11b9467446cb5970d08610e5836d632d3e6f383730cddd26192af5b90dadcee43',
+//   amount: 100,
+//   signature: '3045022100eb25919bbbc253110557eebdfceb12e5a9bb48ed4f4d5969bbe415d66820d07702201d12566f7c2072a7617eb932ecf18fe94f48ee40deb78208dd87b07f53ea011c'
+
+// There are some fundamental problems here, here aditya is sending the 100 coins to Recutor but Aditya dont have any coin so lets build that here to have coins 
+
+// now created a getBalance method in Blockchain that will do all these 

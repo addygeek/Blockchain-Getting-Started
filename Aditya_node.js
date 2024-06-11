8dc7a8aaa5b3161f38bf5461a175d31e8b5fe29a
addygeek <ui22cs03@iiitsurat.ac.in>
2024-06-12 00:08:44 +0530

    aditya peer to peer node added and one more to communicate

---
+++ Aditya_node.js
@@ -0,0 +1,76 @@
+const WS =require("ws");
+
+const PORT = 3001;
+
+const MY_ADDRESS = "ws://localhost:" + PORT;
+
+const server = new WS.Server({port:PORT});
+
+let opened= [], connected=[];
+
+console.log("Aditya Node is listening on Port " +PORT);
+
+server.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        const _message = JSON.parse(message);
+        console.log(_message);
+        switch (_message.type){
+            case "TYPE_HANDSHAKE":
+                const nodes = _message.data;
+                nodes.forEach(node => connected(node));
+        }
+    });
+})
+
+function connected(address)
+{
+if(!connected.find(peerAddress => peerAddress=== address && address !== MY_ADDRESS))
+{
+    const socket = new WS(address);
+
+    socket.on("open", () => {
+      //  opened.push({address, socket});
+      //peer to peer push 
+        socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE",{MY_ADDRESS, ...connected})));
+        opened.forEach(node => node.socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE",{address}))));
+       
+        if(!opened.find(peer => peer.address === address) && address !== MY_ADDRESS)
+        {
+            opened.push({socket,address});
+            connected.push(address);
+        }
+    
+    });
+
+    socket.on("close", () => {
+        opened.splice(connected.indexOf(address),1);
+        connected.splice(connected.indexOf(address),1);
+    })
+
+function produceMessage(type,data){
+    return {type,data};
+}
+
+setTimeout(() =>{
+sendMessage(produceMessage("MESSAGE", "I am Aditya the NODE!!!"));
+},3000);
+
+function sendMessage(message)
+{
+    opened.forEach(node =>{
+        node.socket.send(JSON.stringify(message));
+    })
+}
+
+    //i 
+    socket.on("close", (message) => {
+        const _message = JSON.parse(message);
+        console.log(_message);
+        switch (_message.type){
+            case "TYPE_HANDSHAKE":
+                const nodes = _message.data;
+                nodes.forEach(node => connected(node));
+        }
+    });
+}
+}
